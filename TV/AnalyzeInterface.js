4b2e97016ecf36744ef35e4d573d24cddea71d5b
xtxk110 <329986833@qq.com>
2019-02-12 14:51:04 +0800

    d

---
+++ TV/AnalyzeInterface.js
@@ -0,0 +1,16 @@
+﻿//获取接口数据
+function GetInterfaceData(){
+	var data=[          "https://jx.618g.com/?url=", 
+						"http://api.smq1.com/?url=",
+						"http://api.51ckm.com/Box.php?url=",
+						"http://api.zuilingxian.com/jiexi.php?url=",
+						"http://api.baiyug.cn/vip/?url=",   
+						"https://jx.km58.top/jx/?url=",
+						"https://cdn.yangju.vip/k/?url=",
+						"http://www.wmxz.wang/video.php?url=",
+						"http://jqaaa.com/jx.php?url="
+						
+			];
+	return data;
+}
+
---
+++ TV/Web.js
@@ -0,0 +1,18 @@
+﻿//获取影视网站数据
+function GetWebUrl(){
+	var data=[{name:"爱奇艺影视",url:"http://m.iqiyi.com/"},
+                  {name:"腾讯视频",url:"https://m.v.qq.com/"},
+                  {name:"优酷视频",url:"http://www.youku.com/"},
+                  {name:"搜狐视频",url:"https://m.tv.sohu.com/"},
+                  {name:"乐视视频",url:"https://m.le.com/"},
+                  {name:"365电影",url:"https://www.365dy.cn/#"},
+                  {name:"88影视",url:"https://m.88ys.cn/#"},
+                  {name:"全民影院",url:"http://www.633tv.com/#"}
+	         ];
+	return data;
+}
+//http://www.gotopsports.com/Content/TV.apk  https://github.com/xtxk110/All/raw/master/TV.apk
+var obj={
+         ver:3.9,
+	 url:"https://github.com/xtxk110/All/raw/master/TV.apk"
+	};
