15020e221a3d0ff584ea4bb310ee57c851cd6f74
xtxk110 <329986833@qq.com>
2019-02-12 16:47:07 +0800

    d

--- TV/Web.js
+++ TV/Web.js
@@ -13,6 +13,6 @@ function GetWebUrl(){
 }
 //http://www.gotopsports.com/Content/TV.apk  https://github.com/xtxk110/All/raw/master/TV.apk
 var obj={
-         ver:3.92,
+         ver:3.96,
 	 url:"https://github.com/xtxk110/WebApp_TV/raw/master/TV/unpackage/release/TV.apk"
 	};
