dc0d7aa20efee5d972f8f482868003f2d538b4d4
xtxk110 <329986833@qq.com>
2019-03-05 15:59:10 +0800

    增加过滤三方网广告功能

--- TV/AnalyzeInterface.js
+++ TV/AnalyzeInterface.js
@@ -1,9 +1,10 @@
 ﻿//获取接口数据
 function GetInterfaceData(){
-	var data=[    		"http://vip.ooosoo.com/jx/?url=",      
+	var data=[    		     
 						"https://jx.618g.com/?url=",
 						"http://api.smq1.com/?url=",
 						"http://api.51ckm.com/Box.php?url=",
+						"http://vip.ooosoo.com/jx/?url=", 
 						"http://api.zuilingxian.com/jiexi.php?url=",
 						"http://api.baiyug.cn/vip/?url=",   
 						"https://jx.km58.top/jx/?url=",
@@ -14,4 +15,12 @@ function GetInterfaceData(){
 			];
 	return data;
 }
-  
+var AdReject=[
+//全民影院网站广告脚本
+{match:"https://js.users.51.la/19598991.js",redirect:"Empty.js"}
+,{match:"https://s23.cnzz.com/z_stat.php?id=1276226633&web_id=1276226633",redirect:"Empty.js"}
+,{match:"https://c.cnzz.com/core.php?web_id=1276226633&t=z",redirect:"Empty.js"}
+,{match:"http://y.yeaimm.com/81960",redirect:"Empty.js"}
+// 88影视网 https://m.88ys.cn/ 
+,{match:"https://kim.lfhnkp.com/5380.*",redirect:"Empty.js"}
+]
