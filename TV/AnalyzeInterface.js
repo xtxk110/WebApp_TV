2a4a536d549b47d45d21029eee9c6d007efffbbf
xtxk110 <329986833@qq.com>
2019-03-07 14:35:29 +0800

    dd

--- TV/AnalyzeInterface.js
+++ TV/AnalyzeInterface.js
@@ -16,6 +16,7 @@ function GetInterfaceData(){
 	return data;
 }
 var AdReject=[
+
 //全民影院网站广告脚本
 {match:"https://js.users.51.la/19598991.js",redirect:"Empty.js"}
 ,{match:"https://s23.cnzz.com/z_stat.php?id=1276226633&web_id=1276226633",redirect:"Empty.js"}
@@ -25,6 +26,7 @@ var AdReject=[
 ,{match:"https://kim.lfhnkp.com/5380.*",redirect:"Empty.js"}
 ]
 
+//清除广告脚本
 var AdJs=""+
 "function ClearAd(){"+
 	"var d=document.querySelectorAll('[style*=z-index]');"+
@@ -36,5 +38,7 @@ var AdJs=""+
 		"}"+	
 	"}"+
 "}"+
-"setTimeout(function(){ClearAd();},1000);"+
+"setTimeout(function(){ClearAd();},500);"+
 "";
+
+var ClearAll=true;//是所有网页启用去除广告功能(true),还是只对带?no的链接启用去除广告功能(false)
