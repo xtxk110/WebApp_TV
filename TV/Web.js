8e614d94dcf3e5a4def04195d630669a3c3109a6
xtxk110 <329986833@qq.com>
2019-03-07 16:05:02 +0800

    ddd

--- TV/Web.js
+++ TV/Web.js
@@ -1,4 +1,4 @@
-﻿//获取影视网站数据
+//获取影视网站数据
 function GetWebUrl(){
 	var data=[{name:"爱奇艺影视",url:"http://m.iqiyi.com/"},
                   {name:"腾讯视频",url:"https://m.v.qq.com/"},
@@ -13,7 +13,7 @@ function GetWebUrl(){
 }
 //http://www.gotopsports.com/Content/TV.apk  https://github.com/xtxk110/All/raw/master/TV.apk
 var obj={
-         ver:5.9,
+         ver:6.0,
 	 url:"https://github.com/xtxk110/WebApp_TV/raw/master/TV/unpackage/release/TV.apk",
 	 live:"http://633tv.com"
 	};
