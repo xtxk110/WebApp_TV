59580bd5f1459ad0638fe6e5b613f62d6aba55a8
xtxk110 <329986833@qq.com>
2019-03-31 21:23:16 +0800

    ff

--- TV/Web.js
+++ TV/Web.js
@@ -5,9 +5,9 @@ function GetWebUrl(){
                   {name:"优酷视频",url:"http://www.youku.com/"},
                   {name:"搜狐视频",url:"https://m.tv.sohu.com/"},
                   {name:"乐视视频",url:"https://m.le.com/"},
-                  {name:"全民电影网",url:"https://www.qmndy.com/#?no"},
-                  {name:"88影视",url:"https://m.88ys.cn/#"},
-                  {name:"全民影院",url:"http://www.70bt.cn/#"}
+                  {name:"全民电影网",url:"https://www.qmndy.com/#"},
+                  {name:"9527影视",url:"http://www.9527yy.com/#"},
+                  {name:"全民影院",url:"http://www.70bt.cn/#?no"}
 	         ];
 	return data;
 }
