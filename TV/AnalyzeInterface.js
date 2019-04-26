0a3b69671a495224def47540e5b1654b4fab4bdf
xtxk110 <329986833@qq.com>
2019-04-26 20:56:46 +0800

    ll

--- TV/AnalyzeInterface.js
+++ TV/AnalyzeInterface.js
@@ -1,9 +1,11 @@
 ﻿//获取接口数据
 function GetInterfaceData(){
-	var data=[    		     
+	var data=[   
+						"http://55jx.top/?url=", 
 						"https://jx.618g.com/?url=",
+						"https://z1.m1907.cn/?jx=",
+						"http://jx.618g.com/?url=",
 						"http://api.smq1.com/?url=",
-                        "https://z1.m1907.cn/?jx=",
 						"http://api.zuilingxian.com/jiexi.php?url=",
                         "http://api.51ckm.com/Box.php?url=",
 						"http://vip.ooosoo.com/jx/?url="
--- TV/Web.js
+++ TV/Web.js
@@ -1,6 +1,6 @@
 //获取影视网站数据
 function GetWebUrl(){
-	var data=[{name:"爱奇艺影视",url:"http://m.iqiyi.com/"},
+	var data=[{name:"爱奇艺影视",url:"https://m.iqiyi.com/"},
                   {name:"腾讯视频",url:"https://m.v.qq.com/"},
                   {name:"优酷视频",url:"https://www.youku.com/"},
                   {name:"搜狐视频",url:"https://m.tv.sohu.com/"},
