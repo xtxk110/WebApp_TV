81ca010d7ca57e2ad0952a8ae1ca84335ed9629d
xtxk110 <329986833@qq.com>
2019-07-08 12:19:50 +0800

    s

--- TV/AnalyzeInterface.js
+++ TV/AnalyzeInterface.js
@@ -8,32 +8,12 @@ function GetInterfaceData(){
 			];
 	return data;
 }
-var AdReject=[
 
+//广告网址,支持正则表达式
 //全民影院网站广告脚本
-{match:"https://js.users.51.la/19598991.js",redirect:"Empty.js"}
-,{match:"https://s23.cnzz.com/z_stat.php?id=1276226633&web_id=1276226633",redirect:"Empty.js"}
-,{match:"https://c.cnzz.com/core.php?web_id=1276226633&t=z",redirect:"Empty.js"}
-,{match:"http://y.yeaimm.com/.*",redirect:"Empty.js"}
-,{match:"https://m.puzwb.cn/.*",redirect:"Empty.js"}
-,{match:"https://m.kfuih.cn/.*",redirect:"Empty.js"}
-// 88影视网 https://m.88ys.cn/ 
-,{match:"https://kim.lfhnkp.com/5380.*",redirect:"Empty.js"}
+var AdReject=[
+"https://kim.lfhnkp.com/5380.*"
 ]
 
-//清除广告脚本  z>=2147480000 "break;"+
-var AdJs=""+
-"function ClearAd(){"+
-	"var d=document.querySelectorAll('[style*=z-index],[style*=position]');"+
-	"for (i=0;i<d.length;i++) {"+
-		"var z=d[i].style.zIndex;var s=d[i].style"+
-		"if(z>=100000||((s.position=='fixed||s.position=='absolute')'&&(s.bottom==0||s.bottom=='0px'))){"+
-			"d[i].style.display='none';"+
-			
-		"}"+	
-	"}"+
-"}"+
-"setTimeout(function(){ClearAd();},500);"+
-"";
 
 var ClearAll=true;//是所有网页启用去除广告功能(true),还是只对带?no的链接启用去除广告功能(false)
